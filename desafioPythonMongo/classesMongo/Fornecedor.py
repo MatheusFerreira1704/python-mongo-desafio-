c8b6ac00d21e8c1fe6e669483740fd8b58edc1f7
Matheus Ferreira da Silva <mattheus1704@gmail.com>
2021-02-28 17:22:05 -0300

    Fornecedores commit
    Inserido os restantes das funções do CRUD de fornecedores.

--- desafioPythonMongo/classesMongo/Fornecedor.py
+++ desafioPythonMongo/classesMongo/Fornecedor.py
@@ -11,7 +11,7 @@ banco = bdCliente['MongoVendas'] #referenciação ao banco.
 
 class Fornecedores(object):
 
-    def inserir(self):
+    def inserir(self): #persistência do documento fornecedor na collection.
         fornecedor = {
             'razao_social': self.razao_social,
             'cnpj': self.cnpj,
@@ -23,7 +23,42 @@ class Fornecedores(object):
         # time.sleep(1)
         print('Fornecedor inserido com sucesso...')
 
-    def referenciador(self, codigoCNPJ):
+    def listarFornecedores(self): #função que mostra a lista de fornecedores cadastrados.
+        fornecedores = banco.fornecedores
+        for doc in fornecedores.find():
+            print(f"Razão Social: {doc['razao_social']}\nCNPJ: {doc['cnpj']}\nRepresentante: {doc['representante']}\nTelefone: {doc['telefone']}\n")
+
+    def consultarFornecedores(self, cnpj): #função que busca um fornecedor especifico.
+        fornecedores = banco.fornecedores
+        doc = fornecedores.find_one({'cnpj':cnpj})
+        if doc is None:
+            print('Fornecedor não encontrado.')
+        else:
+            print(f"Razão Social: {doc['razao_social']}\nCNPJ: {doc['cnpj']}\nRepresentante: {doc['representante']}\nTelefone: {doc['telefone']}\n")
+
+    def atualizarFornecedor(self, cnpj, dado, valor): #função de atualização de informações dos fornecedores cadastrados.
+        fornecedor = banco.fornecedores
+        doc = fornecedor.find_one({'cnpj': cnpj})
+        if doc is None:
+            print('Fornecedor não encontrado.')
+        else:
+            fornecedor.update_one({'cnpj': cnpj},{
+                "$set": {
+                    f'{dado}': valor
+                }
+            })
+
+    def removerFornecedor(self, cnpj): #função de remoção do documento fornecedor da collection.
+        fornecedor = banco.fornecedores
+        doc = fornecedor.find_one({'cnpj': cnpj})
+        if doc is None:
+            print('Fornecedor não encontrado.')
+        else:
+            fornecedor.delete_one(doc)
+            print('Fornecedor removido com sucesso.')
+
+    def referenciador(self, codigoCNPJ): #função que referência o documento do fornecedor na hora da inserir
+        # um documento vento na collection de venda.
         fornecedores = banco.fornecedores
         doc = fornecedores.find_one({'cnpj': codigoCNPJ})
         print('Buscando fornecedor')
